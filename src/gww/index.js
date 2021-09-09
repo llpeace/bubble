84f294d4be5cf3df7a9683eff8e20fe40a945275
李孟云 <limengyun02@maoyan.com>
2021-09-09 18:14:33 +0800

    test: 测试chrome与safari的差别

--- src/gww/index.js
+++ src/gww/index.js
@@ -4,11 +4,16 @@ import './index.less';
 
 export default function Test() {
   return (
-    <div className="test-wrapper">
-      <span>查看记录</span>
-      <span>下载明细</span>
-      <span>无需付款</span>
-      <span>重置结算单</span>
-    </div>
+    <>
+      <div className="test-wrapper">
+        <span>查看记录</span>
+        <div className="history">
+          <span>下载明细</span>
+          <span>无需付款</span>
+          <span>重置结算单</span>
+        </div>
+      </div>
+      <div className="test-div">测试</div>
+    </>
   );
 }
